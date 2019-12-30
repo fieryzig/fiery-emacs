860726ac78600cd1eb8b0af1a528970773afcbae
fieryzig <duo0702@live.cn>
2019-12-31 01:59:31 +0800

    add themes

--- custom.el
+++ custom.el
@@ -3,13 +3,43 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(custom-safe-themes
    (quote
-    ("e7666261f46e2f4f42fd1f9aa1875bdb81d17cc7a121533cad3e0d724f12faf2" default)))
+    ("dc677c8ebead5c0d6a7ac8a5b109ad57f42e0fe406e4626510e638d36bcc42df" "e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" "e7666261f46e2f4f42fd1f9aa1875bdb81d17cc7a121533cad3e0d724f12faf2" default)))
+ '(fci-rule-color "#5B6268")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(make-backup-files nil)
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (company counsel use-package)))
- '(tool-bar-mode nil))
+ '(objed-cursor-color "#ff6c6b")
+ '(package-selected-packages (quote (dashboard company counsel use-package)))
+ '(pdf-view-midnight-colors (cons "#282c34" "#bbc2cf"))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#282c34")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,8 +1,47 @@
+;; Speed up startup
+(defvar centaur-gc-cons-threshold (if (display-graphic-p) 8000000 800000)
+  "The default value to use for `gc-cons-threshold'. If you experience freezing,
+decrease this. If you experience stuttering, increase this.")
+
+(defvar centaur-gc-cons-upper-limit (if (display-graphic-p) 400000000 100000000)
+  "The temporary value for `gc-cons-threshold' to defer it.")
+
+(defvar centaur-gc-timer (run-with-idle-timer 10 t #'garbage-collect)
+  "Run garbarge collection when idle 10s.")
+
+(defvar default-file-name-handler-alist file-name-handler-alist)
+
+(setq file-name-handler-alist nil)
+(setq gc-cons-threshold centaur-gc-cons-upper-limit)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    "Restore defalut values after startup."
+	    (setq file-name-handler-alist default-file-name-handler-alist)
+	    (setq gc-cons-threshold centaur-gc-cons-threshold)
+
+	    ;; GC automatically while unfocusing the frame
+	    ;; `focus-out-hook' is obsolete since 27.1
+	    (if (boundp 'after-focus-change-function)
+		(add-function :after after-focus-change-function
+			      (lambda ()
+				(unless (frame-focus-state)
+				  (garbage-collect))))
+	      (add-hook 'focus-out-hook 'garbage-collect))
+
+	    ;; Avoid GCs while using `ivy'/`counsel'/`swiper' and `helm', etc.
+	    ;; @see http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+	    (defun my-minibuffer-setup-hook ()
+	      (setq gc-cons-threshold centaur-gc-cons-upper-limit))
+
+	    (defun my-minibuffer-exit-hook ()
+	      (setq gc-cons-threshold centaur-gc-cons-threshold))
+
+	    (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+	    (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)))
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
-
-(load-theme 'tango-dark)
 
 (require 'init-package)
 
@@ -15,15 +54,34 @@
   :diminish company-mode
   :hook (after-init . global-company-mode))
 
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+;; (use-package dashboard
+;;   :ensure t
+;;   :config
+;;   (dashboard-setup-startup-hook)
+;;   (setq dashboard-items '((recents . 5)
+;; 			  (bookmarks . 5)
+;; 			  (agenda . 5)))
+;;   (setq dashboard-set-heading-icons t)
+;;   (setq dashboard-set-file-icons t)
+;;   (dashboard-modify-heading-icons '((recents . "file-text")
+;; 				    (bookmarks . "book")))
+;;   (setq dashboard-set-navigator t))
+
 (use-package doom-themes
   :ensure t
   :config
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
   (setq doom-themes-enable-bold t
 	doom-themes-enable-italic t)
-  (load-theme 'doom-one t)
+  (load-theme 'tango-dark t)
 
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
   (setq doom-themes-treemacs-theme "doom-colors")
   (doom-themes-treemacs-config)
   (doom-themes-org-config))
+
