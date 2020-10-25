9d5e04b0b6ba0d24f37efa86d290143bc77c4196
fieryzig <duo0702@live.cn>
2020-10-25 16:22:14 +0800

    feat: add ggtags

--- lisp/code/init-cpp.el
+++ lisp/code/init-cpp.el
@@ -12,4 +12,10 @@
     :diminish t
     :init (modern-c++-font-lock-global-mode t)))
 
+(use-package ggtags
+  :hook
+  (c-mode-common . (lambda ()
+                     (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+                       (ggtags-mode 1)))))
+
 (provide 'init-cpp)
