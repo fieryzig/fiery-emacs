89631ae90158b13025af5d8ac1faba96fc29defd
fieryzig <duo0702@live.cn>
2020-10-28 02:20:17 +0800

    feat: add customize group

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -1,6 +1,29 @@
-(setq user-full-name "fieryzig")
-(setq user-email-address "fieryzig@outlook.com")
+;; -*- lexical-binding: t -*-
 
+;; constant
 (defconst *sys/win32*
   (eq system-type 'windows-nt))
+
+;; custom
+(defgroup fiery nil
+  "fiery-emacs custom"
+  :link '(url-link "https://github.com/fieryzig/fiery-emacs"))
+
+(defcustom fiery-full-name "fieryzig"
+  "user-full-name"
+  :group 'fiery
+  :type 'string)
+
+(defcustom fiery-email-address "fieryzig@outlook.com"
+  "user-email-address"
+  :group 'fiery
+  :type 'string)
+
+(defcustom fiery-org-files (expand-file-name "~/org/")
+  "user-org-files"
+  :group 'fiery
+  :type 'string)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 (provide 'init-const)
