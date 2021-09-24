49018293057913176be9f007adc6a1a6173aadc6
zagfang <zagfang@tencent.com>
2021-09-24 12:22:12 +0800

    feature: maximize window when starting up.

--- early-init.el
+++ early-init.el
@@ -7,6 +7,7 @@
   (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; edit emacs config for short
 (defun edit-configs()
