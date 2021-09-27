002225b48f9391be6f3458f9a12bea50f9decb24
fieryzig <duo0702@live.cn>
2021-09-28 03:50:54 +0800

    fix: fix modeline appearance by moving cnfonts into early-init.el.

--- early-init.el
+++ early-init.el
@@ -14,4 +14,37 @@
   (interactive)
   (find-file (concat user-emacs-directory "init.el")))
 
+;; use-package
+(setq package-archives
+      '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+	("melpa" . "http://elpa.emacs-china.org/melpa/")))
+;; (unless (bound-and-true-p package--initialized)
+;;   (setq package-enable-at-startup nil)
+;;   (package-initialize))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t)
+  (setq use-package-expand-minimally t)
+  (setq use-package-compute-statistics t)
+  (setq use-package-enable-imenu-support t))
+
+;; font & icons
+(use-package all-the-icons)
+(use-package cnfonts
+  :after all-the-icons
+  :hook (cnfonts-set-font-finish
+	 . (lambda (fontsizes-list)
+	     (set-fontset-font t 'unicode (font-spec :family "all-the-icons") nil 'append)
+	     (set-fontset-font t 'unicode (font-spec :family "file-icons") nil 'append)
+	     (set-fontset-font t 'unicode (font-spec :family "Material Icons") nil 'append)
+	     (set-fontset-font t 'unicode (font-spec :family "github-octicons") nil 'append)
+	     (set-fontset-font t 'unicode (font-spec :family "FontAwesome") nil 'append)
+	     (set-fontset-font t 'unicode (font-spec :family "Weather Icons") nil 'append)))
+  :init
+  (cnfonts-enable))
+
+
 (provide 'early-init)
--- lisp/init-env.el
+++ lisp/init-env.el
@@ -13,27 +13,7 @@ apps are not started from a shell."
 						    ))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
-
 (set-exec-path-from-shell-PATH)
-
-;; use-package
-(setq package-archives
-      '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-	("melpa" . "http://elpa.emacs-china.org/melpa/")))
-;; (unless (bound-and-true-p package--initialized)
-;;   (setq package-enable-at-startup nil)
-;;   (package-initialize))
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t)
-  (setq use-package-expand-minimally t)
-  (setq use-package-compute-statistics t)
-  (setq use-package-enable-imenu-support t))
-;; (use-package diminish)
-;;
 
 ;; basic config
 (save-place-mode 1)
@@ -63,21 +43,5 @@ apps are not started from a shell."
                      "url"
                      "COMMIT_EDITMSG\\'")))
 (setq-default history-length 500)
-
-
-;; font & icons
-(use-package all-the-icons)
-(use-package cnfonts
-  :after all-the-icons
-  :hook (cnfonts-set-font-finish
-	 . (lambda (fontsizes-list)
-	     (set-fontset-font t 'unicode (font-spec :family "all-the-icons") nil 'append)
-	     (set-fontset-font t 'unicode (font-spec :family "file-icons") nil 'append)
-	     (set-fontset-font t 'unicode (font-spec :family "Material Icons") nil 'append)
-	     (set-fontset-font t 'unicode (font-spec :family "github-octicons") nil 'append)
-	     (set-fontset-font t 'unicode (font-spec :family "FontAwesome") nil 'append)
-	     (set-fontset-font t 'unicode (font-spec :family "Weather Icons") nil 'append)))
-  :init
-  (cnfonts-enable))
 
 (provide 'init-env)
